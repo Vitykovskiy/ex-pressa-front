bae2af0227af77902cc4f03d5afbb049ce927733
Vitykovskiy <vitykovskiy@gmail.com>
2025-11-04 19:25:58 +0300

    Типизация и отрисовка меню

---
+++ src/pages/constants.ts
@@ -0,0 +1,285 @@
+import {
+  type AnyMenuGroup,
+  DrinkSize,
+  MenuGroupType,
+} from "@/services/menu/types";
+import type { DataTableHeader } from "vuetify";
+
+export const MENU_DATA: AnyMenuGroup[] = [
+  {
+    id: 1,
+    type: MenuGroupType.Drinks,
+    name: "Классика кофе",
+    position: 1,
+    items: [
+      {
+        id: 101,
+        name: "Эспрессо",
+        position: 1,
+        isAvailable: true,
+        sizes: {
+          [DrinkSize.Small]: {
+            id: 1011,
+            size: DrinkSize.Small,
+            price: 120,
+          },
+          [DrinkSize.Medium]: {
+            id: 1012,
+            size: DrinkSize.Medium,
+            price: 150,
+          },
+          [DrinkSize.Large]: {
+            id: 1013,
+            size: DrinkSize.Large,
+            price: 180,
+          },
+        },
+      },
+      {
+        id: 102,
+        name: "Американо",
+        position: 2,
+        isAvailable: true,
+        sizes: {
+          [DrinkSize.Small]: {
+            id: 1021,
+            size: DrinkSize.Small,
+            price: 130,
+          },
+          [DrinkSize.Medium]: {
+            id: 1022,
+            size: DrinkSize.Medium,
+            price: 160,
+          },
+          [DrinkSize.Large]: {
+            id: 1023,
+            size: DrinkSize.Large,
+            price: 190,
+          },
+        },
+      },
+      {
+        id: 103,
+        name: "Капучино",
+        position: 3,
+        isAvailable: true,
+        sizes: {
+          [DrinkSize.Small]: {
+            id: 1031,
+            size: DrinkSize.Small,
+            price: 160,
+          },
+          [DrinkSize.Medium]: {
+            id: 1032,
+            size: DrinkSize.Medium,
+            price: 190,
+          },
+          [DrinkSize.Large]: {
+            id: 1033,
+            size: DrinkSize.Large,
+            price: 220,
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: 2,
+    type: MenuGroupType.Drinks,
+    name: "Сладкий кофе",
+    position: 2,
+    items: [
+      {
+        id: 201,
+        name: "Латте карамельный",
+        position: 1,
+        isAvailable: true,
+        sizes: {
+          [DrinkSize.Small]: {
+            id: 2011,
+            size: DrinkSize.Small,
+            price: 180,
+          },
+          [DrinkSize.Medium]: {
+            id: 2012,
+            size: DrinkSize.Medium,
+            price: 210,
+          },
+          [DrinkSize.Large]: {
+            id: 2013,
+            size: DrinkSize.Large,
+            price: 240,
+          },
+        },
+      },
+      {
+        id: 202,
+        name: "Мокка",
+        position: 2,
+        isAvailable: true,
+        sizes: {
+          [DrinkSize.Small]: {
+            id: 2021,
+            size: DrinkSize.Small,
+            price: 190,
+          },
+          [DrinkSize.Medium]: {
+            id: 2022,
+            size: DrinkSize.Medium,
+            price: 220,
+          },
+          [DrinkSize.Large]: {
+            id: 2023,
+            size: DrinkSize.Large,
+            price: 250,
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: 3,
+    type: MenuGroupType.Drinks,
+    name: "Не кофе",
+    position: 3,
+    items: [
+      {
+        id: 301,
+        name: "Матча латте",
+        position: 1,
+        isAvailable: true,
+        sizes: {
+          [DrinkSize.Small]: {
+            id: 3011,
+            size: DrinkSize.Small,
+            price: 200,
+          },
+          [DrinkSize.Medium]: {
+            id: 3012,
+            size: DrinkSize.Medium,
+            price: 230,
+          },
+        },
+      },
+      {
+        id: 302,
+        name: "Какао",
+        position: 2,
+        isAvailable: true,
+        sizes: {
+          [DrinkSize.Small]: {
+            id: 3021,
+            size: DrinkSize.Small,
+            price: 170,
+          },
+          [DrinkSize.Medium]: {
+            id: 3022,
+            size: DrinkSize.Medium,
+            price: 200,
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: 4,
+    type: MenuGroupType.Food,
+    name: "Десерты",
+    position: 4,
+    items: [
+      { id: 401, name: "Чизкейк", price: 250, position: 1, isAvailable: true },
+      { id: 402, name: "Брауни", price: 220, position: 2, isAvailable: true },
+      {
+        id: 403,
+        name: "Пирог с яблоками",
+        price: 230,
+        position: 3,
+        isAvailable: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    type: MenuGroupType.Food,
+    name: "Еда",
+    position: 5,
+    items: [
+      {
+        id: 501,
+        name: "Сэндвич с курицей",
+        price: 270,
+        position: 1,
+        isAvailable: true,
+      },
+      {
+        id: 502,
+        name: "Круассан с ветчиной и сыром",
+        price: 260,
+        position: 2,
+        isAvailable: true,
+      },
+    ],
+  },
+  {
+    id: 6,
+    type: MenuGroupType.Other,
+    name: "Доп. опции",
+    position: 6,
+    items: [
+      {
+        id: 601,
+        name: "Молоко безлактозное",
+        price: 30,
+        position: 1,
+        isAvailable: true,
+      },
+      {
+        id: 602,
+        name: "Доп. шот эспрессо",
+        price: 50,
+        position: 2,
+        isAvailable: true,
+      },
+      { id: 603, name: "Сироп", price: 20, position: 3, isAvailable: true },
+    ],
+  },
+  {
+    id: 7,
+    type: MenuGroupType.Other,
+    name: "Мерч",
+    position: 7,
+    items: [
+      {
+        id: 701,
+        name: "Кружка фирменная",
+        price: 600,
+        position: 1,
+        isAvailable: true,
+      },
+      {
+        id: 702,
+        name: "Термокружка",
+        price: 900,
+        position: 2,
+        isAvailable: true,
+      },
+      { id: 703, name: "Худи", price: 2500, position: 3, isAvailable: true },
+    ],
+  },
+];
+
+export const DRINKS_TABLE_HEADERS: DataTableHeader[] = [
+  { title: "Наименование", key: "name", sortable: false },
+  { title: "S", key: "sizes.S.price", align: "end", sortable: false },
+  { title: "M", key: "sizes.M.price", align: "end", sortable: false },
+  { title: "L", key: "sizes.L.price", align: "end", sortable: false },
+];
+
+export const OTHER_TABLE_HEADERS: DataTableHeader[] = [
+  { title: "Наименование", key: "name", sortable: false },
+  { title: "Цена", key: "price", align: "end", sortable: false },
+];
+
+export const GROUPS_TABLE_HEADERS: DataTableHeader[] = [
+  { title: "Наименование", key: "name", sortable: false },
+];
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,79 +1,100 @@
 <template>
   <div class="menu">
     <div class="menu__header">
-      <v-btn variant="plain" color="primary" icon="mdi-cart-variant" />
+      <div class="menu__header-left">
+        <v-btn v-if="activeGroup" variant="plain" icon="mdi-arrow-left" @click="goToMenu" />
+      </div>
+      <div class="menu__header-right">
+        <v-btn variant="plain" color="primary" icon="mdi-cart-variant" />
+      </div>
     </div>
-    <h2 class="menu__title">{{ title }}</h2>
+
     <div class="menu__content">
-      <v-data-table hide-default-footer :items-per-page="-1" :items="menu" :headers="headers" />
+      <v-data-table hide-default-footer :items-per-page="-1" :items="items" :headers="headers" hover
+        @click:row="onRowClick">
+        <template v-slot:header.name>
+          <h2 class="menu__title">{{ title }}</h2>
+        </template>
+      </v-data-table>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onBeforeMount, ref } from 'vue';
-import type { HttpError } from '@/services/http';
-import menuService from '@/services/menu';
-import type { MenuItem } from '@/services/menu/types';
-import type { DataTableHeader } from 'vuetify';
+import { onBeforeMount, ref, computed } from "vue";
+import type { HttpError } from "@/services/http";
+import {
+  isMenuGroup,
+  isMenuItem,
+  MenuGroupType,
+  type AnyMenuGroup,
+  type TableRow,
+} from "@/services/menu/types";
+import type { DataTableHeader } from "vuetify";
+import { DRINKS_TABLE_HEADERS, GROUPS_TABLE_HEADERS, MENU_DATA, OTHER_TABLE_HEADERS } from "./constants";
+import type { ItemSlotBase } from "vuetify/lib/components/VDataTable/types.mjs";
 
-const title = ref('Меню')
-const menu = ref<MenuItem[]>([
-  {
-    "id": 1,
-    "name": "Кофе",
-    "price": 120,
-    "isAvailable": true,
-    "position": 1,
-    "createdAt": "2025-11-03T12:59:40.000Z",
-    "updatedAt": "2025-11-03T12:59:40.000Z"
-  },
-  {
-    "id": 3,
-    "name": "Кофе",
-    "price": 120,
-    "isAvailable": true,
-    "position": 1,
-    "createdAt": "2025-11-03T13:40:07.000Z",
-    "updatedAt": "2025-11-03T13:40:07.000Z"
-  },
-  {
-    "id": 2,
-    "name": "Чай",
-    "price": 100,
-    "isAvailable": true,
-    "position": 2,
-    "createdAt": "2025-11-03T12:59:40.000Z",
-    "updatedAt": "2025-11-03T12:59:40.000Z"
-  },
-  {
-    "id": 4,
-    "name": "Чай",
-    "price": 100,
-    "isAvailable": true,
-    "position": 2,
-    "createdAt": "2025-11-03T13:40:07.000Z",
-    "updatedAt": "2025-11-03T13:40:07.000Z"
+const menu = ref<AnyMenuGroup[]>(MENU_DATA);
+
+const activeGroup = ref<AnyMenuGroup | null>(null);
+
+const title = computed(() =>
+  activeGroup.value ? activeGroup.value.name : "Меню",
+);
+
+const items = computed<TableRow[]>(() =>
+  activeGroup.value ? activeGroup.value.items : menu.value
+);
+
+const headers = computed<DataTableHeader[]>(() => {
+  if (!activeGroup.value) {
+    return GROUPS_TABLE_HEADERS
   }
-])
 
-const headers = ref<DataTableHeader[]>([
-  { title: 'Наименование', key: 'name', sortable: false },
-  { title: 'S', key: 'price', align: 'end', sortable: false },
-  { title: 'M', key: 'price', align: 'end', sortable: false },
-  { title: 'L', key: 'price', align: 'end', sortable: false }
-])
+  switch (activeGroup.value.type) {
+    case MenuGroupType.Drinks:
+      return DRINKS_TABLE_HEADERS;
 
+    case MenuGroupType.Food:
+      return OTHER_TABLE_HEADERS;
+
+    case MenuGroupType.Other:
+      return OTHER_TABLE_HEADERS;
+
+    default:
+      return GROUPS_TABLE_HEADERS;
+  }
+});
+
+function onRowClick(
+  _event: MouseEvent,
+  ctx: ItemSlotBase<TableRow>,
+): void {
+  const tableRow = ctx.item;
+
+  if (!activeGroup.value && isMenuGroup(tableRow)) {
+    activeGroup.value = tableRow;
+    return;
+  }
+
+  if (activeGroup.value && isMenuItem(tableRow)) {
+    console.log("clicked menu item", tableRow);
+  }
+}
+
+function goToMenu(): void {
+  activeGroup.value = null
+}
 
 onBeforeMount(async () => {
   try {
-    /*  menu.value = await menuService.fetchMenu(); */
+    // menu.value = await menuService.fetchMenu();
+    // activeGroup.value = menu.value[0] ?? null;   // если надо сразу открыть первую группу
   } catch (e) {
     const err = e as HttpError;
     console.error(err.message, err.status, err.data);
   }
-})
-
+});
 </script>
 
 <style lang="scss" scoped>
@@ -84,11 +105,7 @@ onBeforeMount(async () => {
 
   &__header {
     display: flex;
-    justify-content: end;
-  }
-
-  &__title {
-    padding: 5px 20px;
+    justify-content: space-between;
   }
 
   &__content {
