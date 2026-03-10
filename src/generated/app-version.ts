4d40c9f926f8a3b7702dbc0a33ccb57a28f16694
Vitykovskiy <vitykovskiy@gmail.com>
2026-03-10 16:31:48 +0300

    fix: deploy prod from fetch head
    Version: 0.0.0+20260310.133148

--- src/generated/app-version.ts
+++ src/generated/app-version.ts
@@ -1,7 +1,7 @@
-export const APP_VERSION = "0.0.0+20260310.132318";
+export const APP_VERSION = "0.0.0+20260310.133148";
 export const APP_VERSION_INFO = {
   appName: "ex-pressa-front",
   packageVersion: "0.0.0",
   version: APP_VERSION,
-  generatedAt: "2026-03-10T13:23:18.155Z",
+  generatedAt: "2026-03-10T13:31:48.290Z",
 } as const;
